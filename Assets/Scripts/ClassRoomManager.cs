885f6e20106af83cd8d60e04dbb49c352e988bf8
Galalmounir <ghassan@cs.queensu.ca>
2015-04-18 17:17:40 -0400

    firstProjectUpload

---
+++ Assets/Scripts/ClassRoomManager.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ClassRoomManager : MonoBehaviour {
+
+
+	public UnityEngine.UI.Image[] posts;
+	public GameObject manager;
+	public int currentStudent, currentTarget;
+
+	public void StartFB(int position){
+		int x,y;
+		x = position/4;
+		y = (int)(((position/4.0f)-x)/ 0.25f);
+		currentStudent = position;
+
+		GameObject student = manager.gameObject.GetComponent<GameManager>().classroom[x,y];
+		GameObject fb = this.transform.FindChild("FB").gameObject;
+		fb.SetActive(true);
+		fb.transform.FindChild("Name").GetComponent<UnityEngine.UI.Text>().text = student.GetComponent<Personality>().name;
+		fb.transform.FindChild("Profile Pic").FindChild("Pic").GetComponent<UnityEngine.UI.Image>().sprite = student.GetComponent<UnityEngine.UI.Image>().sprite;
+		fb.transform.FindChild("About").FindChild("Birthday").GetComponent<UnityEngine.UI.Text>().text = student.GetComponent<Personality>().birthday;
+
+		Debug.Log(x.ToString()+y.ToString());
+
+		for (int i=0; i<posts.Length;i++){
+			posts[i].gameObject.transform.FindChild("Post Text").GetComponent<UnityEngine.UI.Text>().text = student.GetComponent<Personality>().posts[i];
+			posts[i].gameObject.transform.FindChild("Post Name").GetComponent<UnityEngine.UI.Text>().text = student.GetComponent<Personality>().name;
+			posts[i].gameObject.transform.FindChild("Comment Image").GetComponent<UnityEngine.UI.Image>().sprite = student.GetComponent<UnityEngine.UI.Image>().sprite;
+			posts[i].gameObject.transform.FindChild("Post Image").GetComponent<UnityEngine.UI.Image>().sprite = student.GetComponent<UnityEngine.UI.Image>().sprite;
+		}
+
+	}
+
+
+	void Action(){
+		int x,y,w,z;
+		x = currentStudent/4;
+		y = (int)(((currentStudent/4.0f)-x)/ 0.25f);
+		w = currentTarget/4;
+		z = (int)(((currentTarget/4.0f)-w)/ 0.25f);
+
+		GameObject student = manager.gameObject.GetComponent<GameManager>().classroom[x,y];
+
+		if(student.GetComponent<Nodes>().moveType == 1 || student.GetComponent<Nodes>().moveType == 3 || student.GetComponent<Nodes>().moveType == 10 )
+			student.GetComponent<Nodes>().callAction(x,y);
+		else 
+			student.GetComponent<Nodes>().callAction(x,y,w,z);
+
+	}
+
+	// Use this for initialization
+	void Start () {
+		Rearrange();
+	}
+
+	public void Rearrange(){
+		for (int i=0; i<=manager.gameObject.GetComponent<GameManager>().classroom.GetUpperBound(0);i++){
+			for(int j=0; j<=manager.gameObject.GetComponent<GameManager>().classroom.GetUpperBound(1);j++){
+				GameObject student = manager.gameObject.GetComponent<GameManager>().classroom[i,j];
+				if (student.gameObject.tag == "Student"){
+					int index = i*4 + j;
+					student.gameObject.transform.SetSiblingIndex(index);
+					student.gameObject.GetComponent<UnityEngine.UI.Button>().onClick.AddListener(() => {StartFB(index);});
+				}
+			}
+		}
+	}
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,235 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+enum Turn
+{
+	YourTurn,
+	EnemyTurn,
+	Neither
+};
+enum WinState
+{
+	Win,
+	Lose,
+	Tie,
+	Continue
+};
+
+public class GameManager : MonoBehaviour {
+
+	private static GameManager instance;
+	public static GameManager Instance{
+		get{
+			return instance;
+		}
+	}
+
+	public GameObject[,] classroom;
+	List<string> lastNames = new List<string> { 
+		"Stefan","Soria","Quesenberry","Silvernail","Turrentine",
+		"Jasik","Tutor","Ipock","Ayotte","Stainbrook",
+		"Velazco","Branch","Ericson","Boozer","Graham",
+		"Vanleer","Doherty","Mccargo","Fane","Loch",
+		"Mcnerney","Capps","Keim","Metoyer","Lisle",
+		"Towsend","Lager","Cosenza","Visser","Corrado",
+		"Almendarez","Amerson","Byrd","Napoli","Garduno",
+		"Labrie","Grass","Nicodemus","Kilmer","Yee",
+		"Meserve","Aye","Lomanto","Luck","Verge", 
+		"Roy","Duffy","Jacobus","Johansson","Villeda"};
+	List<string> firstNames = new List<string> { 
+		"Arianna","Shawna","Damian","Stanford","Benton",  
+		"Elly","Venice","Maricruz","Retha","Jacqulyn",
+		"Germaine","Terra","Chasity","Denna","Sabina", 
+		"Diedre","Dallas","Allene","Moises","Christie",  
+		"Kum","Lenard","Leah","Felipe","Leif",
+		"Edelmira","Oscar","Devora","Dayle","Jenise", 
+		"Mavis","Sasha","Kaycee","Linh","Wendell",
+		"Malcom","Jude","Cletus","Ernesto","Lela",
+		"Peggy","Tom","Agatha","Brandie","Lyman",
+		"Leida","Celesta","Mariella","Azalee","Alden"};
+	List<string> moves = new List<string> { 
+		"HangOut","TalkUp","TrashTalk","Introduce","Immunity",
+		"Motivate","PeerPressure","Prank","LiquidCourage","StudySession"};
+	List<string> posts = new List<string> {
+		"Do you prefer ___ or ___?","This or that? This, this or that?",
+		"The best photograph I ever took was ____.","The worst photograph I ever took was ____.",
+		"If I could snap my fingers and be any person in history, I’d be ___.",
+		"By a show of likes, how many of you ______?",
+		"What did you think of the Superbowl/Grammy’s/other event?",
+		"If I could go back in time to my previous self, I’d visit age ___.",
+		"Two things I cannot live without are ____ and ____.",
+		"You’re stranded on a desert island with only one item. Look to your left right now. The first object you see is that item. What is it?",
+		"I love my boyfriend so much! Hearts! Stars! Unicorns!",
+		"Fool me once, shame on you. Fool me twice, shame on me. Revenge will never taste sweeter",
+		"Here goes nothin'!","This sucks...","UGH... SO STRESSED!!! :(",
+		"OMG... I'm so EXCITED!!!",	"Well, I'll never do that again",
+		"I can't believe this is happening","...SIGH...",":-(",
+		" I have the best friends ever!" ," going out!"," school dance tomorrow!",
+		" ______ is the best person ever!"," I'm bored, anyone want to hang?",
+		" I'm sick :("," can't deal with this anymore..."," I need a real job.",
+		" goig to the movies!"," back from the movies!",
+		" it's so hot out!"," it's so cold out!","____ just broke up with me!" ,"slept in...",
+		" it's snowing!","sun!","rain rain go away..."," RAINBOW!"};
+	int week = 1;
+	Turn turn = Turn.YourTurn;
+	WinState currentState = WinState.Continue;
+	public GameObject enemy;
+	public GameObject player;
+	public GameObject classManager;
+
+	void Awake(){
+		if (instance == null) {
+			instance = this;
+		} else if (instance != this)
+			Destroy (gameObject);
+
+		classroom = new GameObject[4,4];
+		classroom[0,0] = GameObject.FindGameObjectWithTag("Enemy");
+		classroom [classroom.GetUpperBound(0),classroom.GetUpperBound(1)] = GameObject.FindGameObjectWithTag("Player");
+
+		int index = 0;
+		GameObject[] others = GameObject.FindGameObjectsWithTag("Student");
+
+		for (int i =0; i < classroom.GetLength(0); i++) {
+			for (int j =0; j < classroom.GetLength(1); j++) {
+				if((i==0&&j==0)||(i==classroom.GetUpperBound(0)&&j==classroom.GetUpperBound(1)))
+					continue;
+				classroom[i,j] = others[index];
+				index++;
+			}
+		}
+	}
+	// Use this for initialization
+	void Start () {
+		//instantiate player nodes
+		enemy = classroom [0,0];
+		enemy.GetComponent<Nodes> ().location (0, 0);
+		//details
+
+		player = classroom [classroom.GetUpperBound(0),classroom.GetUpperBound(1)];
+		player.GetComponent<Nodes> ().location (classroom.GetUpperBound(0),classroom.GetUpperBound(1));
+		//details
+
+		//instantiate all neutral here
+		for (int i =0; i < classroom.GetLength(0); i++) {
+			for (int j =0; j < classroom.GetLength(1); j++) {
+				if((i==0&&j==0)||(i==classroom.GetUpperBound(0)&&j==classroom.GetUpperBound(1)))
+					continue;
+
+				classroom[i,j].tag = "Student";
+				Nodes n = classroom[i,j].GetComponent<Nodes>();
+				Personality p = classroom[i,j].GetComponent<Personality>();
+
+				string randFirst = firstNames[(int)Random.Range(0, Mathf.Round(firstNames.Count - 1))];
+				firstNames.Remove(randFirst);
+				string randLast = lastNames[(int)Random.Range(0, Mathf.Round(lastNames.Count - 1))];
+				lastNames.Remove(randLast);
+				p.name = randFirst+randLast;
+
+				p.posts.Add(posts[(int)Random.Range(0, Mathf.Round(posts.Count - 1))]);
+				p.posts.Add(posts[(int)Random.Range(0, Mathf.Round(posts.Count - 1))]);
+				p.posts.Add(posts[(int)Random.Range(0, Mathf.Round(posts.Count - 1))]);
+				p.posts.Add(posts[(int)Random.Range(0, Mathf.Round(posts.Count - 1))]);
+
+				Actions randAction = (Actions)Random.Range(1, 10);
+				n.GetComponent<Nodes> ().location (i, j);
+				n.GetComponent<Nodes> ().nodeAction(randAction);
+				//same thing for pictures
+			}
+		}
+		SetDefaultScores ();
+	}
+
+	void SetDefaultScores(){
+		classroom [3, 2].GetComponent<Nodes> ().you = 30;
+		classroom [2, 3].GetComponent<Nodes> ().you = 30;
+		classroom [0, 1].GetComponent<Nodes> ().them = 30;
+		classroom [1, 1].GetComponent<Nodes> ().them = 30;
+		classroom [1, 0].GetComponent<Nodes> ().them = 30;
+	}
+
+	void Organize (){
+		turn = Turn.Neither;
+
+		GameObject[] newOrder = GameObject.FindGameObjectsWithTag("Student");
+		newOrder = newOrder.OrderBy (go => (go.GetComponent<Nodes>().totalScore())).ToArray ();
+		int index = 0;
+
+		for (int i =0; i < classroom.GetLength(0); i++) {
+			for (int j =0; j < classroom.GetLength(1); j++) {
+				if((i==0&&j==0)||(i==classroom.GetUpperBound(0)&&j==classroom.GetUpperBound(1)))
+					continue;
+
+				classroom[i,j] = newOrder[index];
+				index++;
+				classroom[i,j].GetComponent<Nodes>().location(i,j);
+
+				//probably some visual update
+			}
+		}
+	}
+
+	void WinConditions(){
+		bool win = false;
+		bool lose = false;
+		for (int i =0; i < classroom.GetLength(0); i++) {
+			for (int j =0; j < classroom.GetLength(1); j++) {
+				if((i==0&&j==0)||(i==classroom.GetUpperBound(0)&&j==classroom.GetUpperBound(1)))
+					continue;
+				win = win || (classroom[i,j].GetComponent<Nodes>().usableP);
+				lose = lose || (classroom[i,j].GetComponent<Nodes>().usableE);
+			}
+		}
+		if (!win && !lose)
+			currentState = WinState.Continue;
+		if (win && !lose)
+			currentState = WinState.Win;
+		if ((win && lose) || week > 12)
+			currentState = WinState.Tie;
+		if (!win && lose)
+			currentState = WinState.Lose;
+	}
+
+
+	void NewTurn(){
+		for (int i =0; i < classroom.GetLength(0); i++) {
+			for (int j =0; j < classroom.GetLength(1); j++) {
+				classroom[i,j].GetComponent<Nodes>().status();
+			}
+		}
+		Organize ();
+		classManager.GetComponent<ClassRoomManager>().Rearrange ();
+	}
+
+	void EnemyTurn(){
+		//enemy.GetComponent<EnemyAI> ().executeNextTurn ();
+	}
+
+	void EndTurn(){
+		week++;
+		WinConditions ();
+		if (currentState != WinState.Continue) {
+			EndGame();
+		}
+	}
+
+	void NextTurn(){
+		EnemyTurn ();
+		EndTurn ();
+		NewTurn ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void EndGame(){
+		var go = new GameObject();
+		GUIText gui = go.AddComponent<GUIText>();
+		go.transform.position.Set (0.5f, 0.5f, 0.0f);
+		gui.text = "Game Over!";
+	}
+}
